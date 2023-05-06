5e5f854437b85db6d96049cd725355e64d2286e9
try <522263680@qq.com>
2023-05-06 17:01:51 +0800

    ising renorm 1

--- test.py
+++ test.py
@@ -1,60 +1,48 @@
+
+
+
+import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Generate some random data
-data = np.random.normal(size=1000)
+# Generate a random graph
+G = nx.erdos_renyi_graph(100, 0.1)
 
-# Calculate the cumulative distribution
-sorted_data = np.sort(data)
-cumulative = np.cumsum(sorted_data)
+# Simulate a diffusion process on the graph
+diffusion = nx.epidemics.diffusion(G, beta=0.1)
 
-# Plot the cumulative distribution
-plt.plot(sorted_data, cumulative)
-plt.title('Cumulative Distribution')
-plt.xlabel('Data')
-plt.ylabel('Cumulative')
+# Plot the graph and the diffusion process
+pos = nx.spring_layout(G)
+nx.draw(G, pos, node_color='lightblue', with_labels=True)
+nx.draw_networkx_nodes(G, pos, nodelist=diffusion[-1], node_color='red')
 plt.show()
 
 
 
+
+import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Generate some random data
-data = np.random.normal(size=1000)
+# Generate a random graph
+G = nx.erdos_renyi_graph(100, 0.1)
 
-# Create histogram
-n, bins, patches = plt.hist(data, bins=30, density=True, cumulative=True)
+# Initialize the state of each node to 0
+state = {node: 0 for node in G.nodes}
 
-# Add labels and title
-plt.xlabel('Value')
-plt.ylabel('Cumulative Probability')
-plt.title('Cumulative Distribution Histogram')
-
-# Show plot
-plt.show()
-plt.legend(loc='lower right')
-
-
-
-
-
-
-def correlation_function(config, k):
-    """
-    Calculate the correlation function for the Ising model.
-    """
-    L = len(config)
-    corr = 0
-    for i in range(L):
-        for j in range(L):
-            corr += config[i][j] * config[(i+k)%L][j]
-    corr /= L**2
-    return corr
+# Simulate a diffusion process on the graph
+for i in range(10):
+    # Update the state of each node based on the majority vote of its neighbors
+    for node in G.nodes:
+        neighbors = list(G.neighbors(node))
+        num_neighbors = len(neighbors)
+        num_ones = sum([state[neighbor] for neighbor in neighbors])
+        if num_ones > num_neighbors / 2:
+            state[node] = 1
+        else:
+            state[node] = 0
+    # Plot the graph and the diffusion process
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, node_color=[state[node] for node in G.nodes], with_labels=True)
+    plt.show()
 
 
-    
-config = [[1, -1, 1], [-1, 1, -1], [1, -1, 1]]
-k = 1
-corr = correlation_function(config, k)
-print(corr)
+
